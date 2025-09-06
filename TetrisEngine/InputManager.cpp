3ed59d68deefbeb3ea1554bcf39c66d22ed6a605
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-06 23:38:58 +0200

    Added basic input system

---
+++ TetrisEngine/InputManager.cpp
@@ -0,0 +1,21 @@
+﻿#include "InputManager.h"
+
+#include <SDL_events.h>
+#include <SDL_ttf.h>
+
+bool monthly::InputManager::Update()
+{
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_QUIT)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+monthly::InputManager::InputManager() = default;
---
+++ TetrisEngine/InputManager.h
@@ -0,0 +1,26 @@
+﻿#ifndef TETRISENGINE_INPUTMANAGER_H
+#define TETRISENGINE_INPUTMANAGER_H
+#include "Singleton.h"
+
+namespace monthly
+{
+	class InputManager final : public Singleton<InputManager>
+	{
+	public:
+		virtual ~InputManager() override = default;
+
+		InputManager(const InputManager& other) = delete;
+		InputManager& operator=(const InputManager& rhs) = delete;
+		InputManager(InputManager&& other) = delete;
+		InputManager& operator=(InputManager&& rhs) = delete;
+
+		bool Update();
+
+	private:
+		friend class Singleton<InputManager>;
+		InputManager();
+
+	};
+}
+
+#endif // TETRISENGINE_INPUTMANAGER_H
