ea2f1e26841609c070f2d4a5b2ea481f65c97c16
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-06 23:38:50 +0200

    Added graphics system base class
    A base class for systems that will use render functionallity. This way we can ensure rendering happens after the update loop.

---
+++ TetrisEngine/GraphicsSystem.h
@@ -0,0 +1,18 @@
+﻿#ifndef TETRISENGINE_GRAPHICSSYSTEM_H
+#define TETRISENGINE_GRAPHICSSYSTEM_H
+
+#include "BaseSystem.h"
+
+namespace monthly
+{
+	class GraphicsSystem : public BaseSystem
+	{
+	public:
+		explicit GraphicsSystem(Registry& pRegistry, uint8_t orderIdx = 1) : BaseSystem(pRegistry, orderIdx) {}
+		virtual ~GraphicsSystem() override = default;
+
+		virtual void Render() {}
+	};
+}
+
+#endif // TETRISENGINE_GRAPHICSSYSTEM_H
