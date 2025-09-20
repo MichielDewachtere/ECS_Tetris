27a719b8ac859f718baf546d8632d989c1acb237
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-20 10:28:54 +0200

    Added basic transform system.
    Updates local position if needed.

---
+++ Tetris/TransformSystem.cpp
@@ -0,0 +1,43 @@
+﻿#include "TransformSystem.h"
+
+#include <Registry.h>
+
+#include "Components.h"
+
+TransformSystem::TransformSystem(monthly::Registry& registry)
+	: BaseSystem(registry, 0)
+{
+}
+
+void TransformSystem::Update()
+{
+	auto idcs = *m_pRegistry->GetDenseMap<PositionComponent>();
+	for (monthly::entity_id idx : idcs)
+	{
+		UpdateLocalPos(idx);
+	}
+}
+
+void TransformSystem::UpdateLocalPos(monthly::entity_id id)
+{
+	PositionComponent* pos = m_pRegistry->GetComponent<PositionComponent>(id).value();
+	if (pos->isDirty == false)
+		return;
+
+	EntityComponent* entity = m_pRegistry->GetComponent<EntityComponent>(id).value();
+	if (entity->parent == monthly::invalid_id)
+	{
+		pos->isDirty = false;
+		return;
+	}
+
+	PositionComponent* parentPos = m_pRegistry->GetComponent<PositionComponent>(entity->parent).value();
+	if (parentPos->isDirty)
+	{
+		UpdateLocalPos(entity->parent);
+	}
+
+	pos->localX = pos->GetGlobalX() - parentPos->GetGlobalX();
+	pos->localY = pos->GetGlobalY() - parentPos->GetGlobalY();
+	pos->isDirty = false;
+}
---
+++ Tetris/TransformSystem.h
@@ -0,0 +1,19 @@
+﻿#ifndef TETRIS_TRANSFORMSYSTEM_H
+#define TETRIS_TRANSFORMSYSTEM_H
+
+#include "BaseSystem.h"
+#include "Entity.h"
+
+class TransformSystem final : public monthly::BaseSystem
+{
+public:
+	TransformSystem(monthly::Registry& registry);
+	virtual ~TransformSystem() override = default;
+
+	virtual void Update() override;
+
+private:
+	void UpdateLocalPos(monthly::entity_id id);
+};
+
+#endif // TETRIS_TRANSFORMSYSTEM_H
