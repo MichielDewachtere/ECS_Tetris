68cdb2149975d4a43b887353a3d438bba4d9144d
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-20 10:29:37 +0200

    Added grid system.

---
+++ Tetris/GridSystem.h
@@ -0,0 +1,18 @@
+﻿#ifndef TETRIS_GRIDSYSTEM_H
+#define TETRIS_GRIDSYSTEM_H
+
+#include <vector>
+
+#include "BaseComponent.h"
+#include "BaseSystem.h"
+
+class GridSystem final : public monthly::BaseSystem
+{
+public:
+	GridSystem(monthly::Registry& registry, uint8_t orderIdx)
+		: BaseSystem(registry, orderIdx) {}
+
+	void Update() override {}
+};
+
+#endif // TETRIS_GRIDSYSTEM_H
