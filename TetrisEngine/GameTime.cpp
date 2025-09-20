72f64e4335c38faa85f6502984ec09a482fb809e
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-20 10:26:57 +0200

    Added time util class

---
+++ TetrisEngine/GameTime.cpp
@@ -0,0 +1,44 @@
+﻿#include "GameTime.h"
+
+void monthly::GameTime::Init()
+{
+	m_PrevTime = std::chrono::high_resolution_clock::now();
+}
+
+void monthly::GameTime::Update()
+{
+	const auto currentTime = std::chrono::high_resolution_clock::now();
+
+	m_DeltaTime = std::chrono::duration<float>(currentTime - m_PrevTime).count();
+	m_TotalTime += m_DeltaTime;
+
+	m_PrevTime = currentTime;
+
+	//FPS LOGIC
+	m_FPSTimer += m_DeltaTime;
+	++m_FPSCount;
+	if (m_FPSTimer >= 1.0f)
+	{
+		m_fFPS = static_cast<float>(m_FPSCount) / m_FPSTimer;
+		m_FPS = m_FPSCount;
+		m_FPSCount = 0;
+		m_FPSTimer = 0.0f;
+	}
+}
+
+float monthly::GameTime::GetElapsed() const
+{
+	return m_DeltaTime;
+}
+
+float monthly::GameTime::GetTotal() const
+{
+	return m_TotalTime;
+}
+
+uint32_t monthly::GameTime::StartTimer()
+{
+	const auto id = m_Timers.size();
+	m_Timers.push_back(std::chrono::high_resolution_clock::now());
+	return static_cast<uint32_t>(id);
+}
---
+++ TetrisEngine/GameTime.h
@@ -0,0 +1,71 @@
+﻿#ifndef TETRISENGINE_GAMETIME_H
+#define TETRISENGINE_GAMETIME_H
+
+#include "Singleton.h"
+
+#include <chrono>
+
+namespace monthly
+{
+	class GameTime final : public Singleton<GameTime>
+	{
+	public:
+		explicit GameTime() = default;
+		virtual ~GameTime() override = default;
+
+		GameTime(const GameTime& other) = delete;
+		GameTime& operator=(const GameTime& rhs) = delete;
+		GameTime(GameTime&& other) = delete;
+		GameTime& operator=(GameTime&& rhs) = delete;
+
+		void Init();
+		void Update();
+
+		float GetElapsed() const;
+		float GetTotal() const;
+		uint32_t GetFPS_Unsigned() const { return m_FPS; };
+		float GetFPS_Float() const { return m_fFPS; }
+
+		[[nodiscard]] uint32_t StartTimer();
+
+		template<typename DurationType = std::chrono::milliseconds>
+		float GetTime(uint32_t id);
+		template<typename DurationType = std::chrono::milliseconds>
+		float EndTimer(uint32_t id);
+
+	private:
+		std::chrono::steady_clock::time_point m_PrevTime{};
+		float m_DeltaTime{};
+		float m_TotalTime{};
+
+		uint32_t m_FPS = 0;
+		float m_fFPS = 0.0f;
+		uint32_t m_FPSCount = 0;
+		float m_FPSTimer = 0.0f;
+
+		std::vector<std::chrono::steady_clock::time_point> m_Timers{};
+	};
+}
+
+template <typename DurationType>
+float monthly::GameTime::GetTime(uint32_t id)
+{
+	const auto endTime = std::chrono::high_resolution_clock::now();
+	const auto duration = endTime - m_Timers[id];
+
+	const float time = static_cast<float>(std::chrono::duration_cast<DurationType>(duration).count());
+	return time;
+}
+
+template<typename DurationType>
+float monthly::GameTime::EndTimer(uint32_t id)
+{
+	const auto time = GetTime<DurationType>(id);
+
+	m_Timers.erase(m_Timers.begin() + id);
+
+	return static_cast<float>(time);
+}
+
+
+#endif // TETRISENGINE_GAMETIME_H
