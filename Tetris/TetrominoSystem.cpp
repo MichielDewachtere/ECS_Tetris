2bb614b2a70ccd9544e60022c131fe146d8349b5
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-24 21:08:46 +0200

    Added Tetromino System to handle the generation of new tetromino's.

---
+++ Tetris/TetrominoSystem.cpp
@@ -0,0 +1,82 @@
+﻿#include "TetrominoSystem.h"
+
+#include <random>
+
+#include "Registry.h"
+
+#include "Components.h"
+#include "MoveSystem.h"
+
+TetrominoSystem::TetrominoSystem(monthly::Registry& registry)
+	: BaseSystem(registry)
+{
+	const auto idcs = *m_pRegistry->GetDenseMap<TetrominoComponent>();
+	for	(const monthly::entity_id id : idcs)
+	{
+		TetrominoComponent* comp = m_pRegistry->GetComponent<TetrominoComponent>(id).value();
+		if (comp->isControlled)
+		{
+			m_pActiveTetrominoComponent = comp;
+			m_pActivePositionComponent = m_pRegistry->GetComponent<PositionComponent>(id).value();
+			m_GridOrigin = glm::ivec2{ m_pActivePositionComponent->GetGlobalX(), m_pActivePositionComponent->GetGlobalY() };
+
+			Tetromino tetromino = GetRandomTetromino();
+			m_pActiveTetrominoComponent->shape = tetromino_to_shape.at(tetromino);
+			m_pActiveTetrominoComponent->color = tetromino_to_color.at(tetromino);
+			m_pActivePositionComponent->IncrementGlobalX(tetromino_to_pos.at(tetromino).x);
+			m_pActivePositionComponent->IncrementGlobalY(tetromino_to_pos.at(tetromino).y);
+		}
+		else
+		{
+			m_pDisplayTetrominoComponent = comp;
+
+			m_DisplayTetromino = GetRandomTetromino();
+			m_pDisplayTetrominoComponent->shape = tetromino_to_shape.at(m_DisplayTetromino);
+			m_pDisplayTetrominoComponent->color = tetromino_to_color.at(m_DisplayTetromino);
+		}
+	}
+
+	const auto moveSystem = m_pRegistry->GetSystem<MoveSystem>();
+	if (moveSystem.has_value())
+	{
+		moveSystem.value()->tetrominoLockedIn.AddObserver(this);
+	}
+}
+
+void TetrominoSystem::Update()
+{
+	if (!m_IsDirty)
+	{
+		return;
+	}
+
+	// TODO: Check if can spawn, if not => game over
+
+	glm::ivec2 tetrominoSpawnPos = m_GridOrigin + tetromino_to_pos.at(m_DisplayTetromino);
+	m_pActivePositionComponent->SetGlobalX(tetrominoSpawnPos.x);
+	m_pActivePositionComponent->SetGlobalY(tetrominoSpawnPos.y);
+
+	m_pActiveTetrominoComponent->shape = tetromino_to_shape.at(m_DisplayTetromino);
+	m_pActiveTetrominoComponent->color = tetromino_to_color.at(m_DisplayTetromino);
+
+	m_DisplayTetromino = GetRandomTetromino();
+	m_pDisplayTetrominoComponent->shape = tetromino_to_shape.at(m_DisplayTetromino);
+	m_pDisplayTetrominoComponent->color = tetromino_to_color.at(m_DisplayTetromino);
+
+	m_IsDirty = false;
+}
+
+void TetrominoSystem::HandleEvent(const glm::ivec2&, TetrominoComponent*)
+{
+	m_IsDirty = true;
+}
+
+TetrominoSystem::Tetromino TetrominoSystem::GetRandomTetromino()
+{
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	static std::uniform_int_distribution<int> dis(0, static_cast<int>(Tetromino::amount) - 1);
+
+	return static_cast<Tetromino>(dis(gen));
+}
+
---
+++ Tetris/TetrominoSystem.h
@@ -0,0 +1,122 @@
+﻿#ifndef TETRIS_TETROMINOSYSTEM_H
+#define TETRIS_TETROMINOSYSTEM_H
+
+#include <unordered_map>
+#include <glm/vec2.hpp>
+#include <glm/ext/vector_uint4_sized.hpp>
+
+#include "BaseSystem.h"
+#include "Observer.h"
+
+struct TetrominoComponent;
+struct PositionComponent;
+
+class TetrominoSystem final
+	: public monthly::BaseSystem
+	, public monthly::Observer<const glm::ivec2&, TetrominoComponent*>
+{
+public:
+	TetrominoSystem(monthly::Registry& registry);
+	virtual ~TetrominoSystem() override = default;
+
+	virtual void Update() override;
+
+	void HandleEvent(const glm::ivec2&, TetrominoComponent*) override;
+	void OnSubjectDestroy() override {}
+
+private:
+	enum Tetromino : uint8_t
+	{
+		I = 0,
+		O = 1,
+		T = 2,
+		S = 3,
+		Z = 4,
+		J = 5,
+		L = 6,
+		amount = 7
+	};
+
+	bool m_IsDirty{ false };
+	glm::ivec2 m_GridOrigin{};
+
+	TetrominoComponent* m_pActiveTetrominoComponent{ nullptr };
+	PositionComponent* m_pActivePositionComponent{ nullptr };
+
+	Tetromino m_DisplayTetromino{};
+	TetrominoComponent* m_pDisplayTetrominoComponent{ nullptr };
+
+	static Tetromino GetRandomTetromino();
+
+	static const inline std::unordered_map<Tetromino, std::vector<std::vector<bool>>> tetromino_to_shape{
+		{ Tetromino::I,
+			{
+				{0,0,0,0},
+				{0,0,0,0},
+				{1,1,1,1},
+				{0,0,0,0},
+			}
+		},
+		{ Tetromino::O,
+			{
+				{1,1},
+				{1,1}
+			}
+		},
+		{ Tetromino::T,
+			{
+				{0,0,0},
+				{1,1,1},
+				{0,1,0},
+			}
+		},
+		{ Tetromino::S,
+			{
+				{0,0,0},
+				{0,1,1},
+				{1,1,0},
+			}
+		},
+		{ Tetromino::Z,
+			{
+				{0,0,0},
+				{1,1,0},
+				{0,1,1},
+			}
+		},
+		{ Tetromino::J,
+			{
+				{0,0,0},
+				{1,1,1},
+				{0,0,1},
+			}
+		},
+		{ Tetromino::L,
+			{
+				{0,0,0},
+				{1,1,1},
+				{1,0,0},
+			}
+		}
+	};
+	static const inline std::unordered_map<Tetromino, glm::ivec2> tetromino_to_pos{
+		{ Tetromino::I, {3,-2} },
+		{ Tetromino::O, {5, 0} },
+		{ Tetromino::T, {4,-1} },
+		{ Tetromino::S, {4,-1} },
+		{ Tetromino::Z, {4,-1} },
+		{ Tetromino::J, {4,-1} },
+		{ Tetromino::L, {4,-1} }
+	};
+	static const inline std::unordered_map<Tetromino, glm::u8vec4> tetromino_to_color{
+	{ Tetromino::I, {0,		255,	255,	255} },
+	{ Tetromino::O, {255,	255,	0,	255} },
+	{ Tetromino::T, {255,	0,	255,	255} },
+	{ Tetromino::S, {0,		255,	0,	255} },
+	{ Tetromino::Z, {255,	0,	0,	255} },
+	{ Tetromino::J, {0	,	0,	255,	255} },
+	{ Tetromino::L, {255,	165,	0,	255} }
+	};
+};
+
+#endif // TETRIS_TETROMINOSYSTEM_H
