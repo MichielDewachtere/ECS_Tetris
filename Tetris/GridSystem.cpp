58cfd3ae659607ff3c520bacaf76e723e9df1cdc
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-24 21:09:19 +0200

    Added Grid System.
    This removes full lines and moves everything down.

--- Tetris/GridSystem.cpp
+++ Tetris/GridSystem.cpp
@@ -1,1 +1,88 @@
 ﻿#include "GridSystem.h"
+
+#include <SDL_rwops.h>
+
+#include "MoveSystem.h"
+#include "Registry.h"
+
+GridSystem::GridSystem(monthly::Registry& registry, monthly::entity_id grid)
+	: BaseSystem(registry)
+{
+	auto moveSystem = m_pRegistry->GetSystem<MoveSystem>();
+	if (moveSystem.has_value())
+	{
+		moveSystem.value()->tetrominoLockedIn.AddObserver(this);
+	}
+
+	auto gridComponent = m_pRegistry->GetComponent<GridComponent>(grid);
+	if (gridComponent.has_value())
+		m_pGridComponent = gridComponent.value();
+}
+
+void GridSystem::HandleEvent(const glm::ivec2& pos, TetrominoComponent* tetromino)
+{
+	auto [rowsToRemove, amountOfRowsToRemove] = FillInGrid(pos, tetromino);
+
+	if (amountOfRowsToRemove == 0)
+	{
+		return;
+	}
+
+	// TODO: Notify HUD.
+	RemoveRows(rowsToRemove);
+}
+
+void GridSystem::OnSubjectDestroy()
+{
+	auto moveSystem = m_pRegistry->GetSystem<MoveSystem>();
+	if (moveSystem.has_value())
+	{
+		moveSystem.value()->tetrominoLockedIn.RemoveObserver(this);
+	}
+}
+
+std::pair<std::array<int, 4>, int> GridSystem::FillInGrid(const glm::ivec2& pos, TetrominoComponent* tetromino) const
+{
+	std::array<int, 4> rowsToRemove{ invalid_row, invalid_row, invalid_row, invalid_row };
+	int amountOfRowsToRemove = 0;
+
+	for (size_t i = 0; i < tetromino->shape.size(); ++i)
+	{
+		for (size_t j = 0; j < tetromino->shape[i].size(); ++j)
+		{
+			if (!tetromino->shape[i][j])
+				continue;
+
+			const size_t gridPosY = pos.y + i;
+			const size_t gridPosX = pos.x + j;
+			auto& grid = m_pGridComponent->grid;
+			grid[gridPosY][gridPosX] = tetromino->color;
+
+			if (std::ranges::find(grid[gridPosY], true, [](const glm::u8vec4& color) -> bool {return color.a == 0;}) == grid[gridPosY].end())
+			{
+				rowsToRemove[i] = static_cast<int>(gridPosY);
+				++amountOfRowsToRemove;
+			}
+		}
+	}
+
+	return { rowsToRemove, amountOfRowsToRemove };
+}
+
+void GridSystem::RemoveRows(const std::array<int, 4>& rowsToRemove) const
+{
+	for (int row : rowsToRemove)
+	{
+		if (row == invalid_row)
+		{
+			continue;
+		}
+
+		for (int y = row; y > 0; --y)
+		{
+			m_pGridComponent->grid[y] = m_pGridComponent->grid[y - 1];
+		}
+
+		m_pGridComponent->grid[0] = std::vector(m_pGridComponent->grid.front().size(), glm::u8vec4{});
+	}
+}
--- Tetris/GridSystem.h
+++ Tetris/GridSystem.h
@@ -2,17 +2,37 @@
 #define TETRIS_GRIDSYSTEM_H
 
 #include <vector>
+#include <array>
+#include <glm/vec2.hpp>
 
-#include "BaseComponent.h"
 #include "BaseSystem.h"
+//#include "Components.h"
+#include "Entity.h"
+#include "Observer.h"
 
-class GridSystem final : public monthly::BaseSystem
+struct GridComponent;
+struct TetrominoComponent;
+
+class GridSystem final
+	: public monthly::BaseSystem
+	, public monthly::Observer<const glm::ivec2&, TetrominoComponent*>
 {
 public:
-	GridSystem(monthly::Registry& registry, uint8_t orderIdx)
-		: BaseSystem(registry, orderIdx) {}
+	GridSystem(monthly::Registry& registry, monthly::entity_id grid);
+	virtual ~GridSystem() override = default;
 
 	void Update() override {}
+
+	void HandleEvent(const glm::ivec2&, TetrominoComponent*) override;
+	void OnSubjectDestroy() override;
+
+private:
+	GridComponent* m_pGridComponent{ nullptr };
+
+	std::pair<std::array<int, 4>, int> FillInGrid(const glm::ivec2& pos, TetrominoComponent* tetromino) const;
+	void RemoveRows(const std::array<int, 4>& rowsToRemove) const;
+
+	static constexpr int invalid_row{ -1 };
 };
 
 #endif // TETRIS_GRIDSYSTEM_H
