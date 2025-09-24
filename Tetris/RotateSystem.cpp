c72ac6d4cdb827ee15ffcd05f82e07389430afef
Michiel Dewachtere <michiel.dewachtere@gmail.com>
2025-09-24 21:08:25 +0200

    Added rotate system to rotate tetromino's

---
+++ Tetris/RotateSystem.cpp
@@ -0,0 +1,49 @@
+﻿#include "RotateSystem.h"
+
+#include "Components.h"
+#include "InputManager.h"
+#include "Registry.h"
+
+RotateSystem::RotateSystem(monthly::Registry& registry, monthly::entity_id activeTetromino, monthly::entity_id grid)
+	: BaseSystem(registry)
+{
+	m_pTetrominoComponent = m_pRegistry->GetComponent<TetrominoComponent>(activeTetromino).value();
+	m_pTetrominoPosComponent = m_pRegistry->GetComponent<PositionComponent>(activeTetromino).value();
+	m_pGridComponent = m_pRegistry->GetComponent<GridComponent>(grid).value();
+}
+
+void RotateSystem::Update()
+{
+	auto& input = monthly::InputManager::GetInstance();
+
+	if (input.IsKeyPressed(SDL_SCANCODE_UP))
+	{
+		RotateShape();
+	}
+}
+
+void RotateSystem::RotateShape() const
+{
+	size_t size = m_pTetrominoComponent->shape.size();
+	std::vector rotatedShape(size, std::vector(size, false));
+
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			rotatedShape[j][size - 1 - i] = m_pTetrominoComponent->shape[i][j];
+
+			const int gridXPos = static_cast<int>(m_pTetrominoPosComponent->GetLocalX() + j);
+			const int gridYPos = static_cast<int>(m_pTetrominoPosComponent->GetLocalY() + size - 1 - i);
+
+			if (gridXPos < 0 || gridXPos >= static_cast<int>(m_pGridComponent->grid.front().size())
+				|| gridYPos >= static_cast<int>(m_pGridComponent->grid.size())
+				|| m_pGridComponent->grid[gridYPos][gridXPos].a)
+			{
+				return;
+			}
+		}
+	}
+
+	m_pTetrominoComponent->shape = rotatedShape;
+}
---
+++ Tetris/RotateSystem.h
@@ -0,0 +1,25 @@
+﻿#ifndef TETRIS_ROTATESYSTEM_H
+#define TETRIS_ROTATESYSTEM_H
+
+#include "BaseSystem.h"
+#include "Entity.h"
+
+struct TetrominoComponent;
+struct PositionComponent;
+struct GridComponent;
+
+class RotateSystem final : public monthly::BaseSystem
+{
+public:
+	RotateSystem(monthly::Registry& registry, monthly::entity_id activeTetromino, monthly::entity_id grid);
+	virtual void Update() override;
+
+private:
+	GridComponent* m_pGridComponent{ nullptr };
+	TetrominoComponent* m_pTetrominoComponent{ nullptr };
+	PositionComponent* m_pTetrominoPosComponent{ nullptr };
+
+	void RotateShape() const;
+};
+
+#endif // TETRIS_ROTATESYSTEM_H
